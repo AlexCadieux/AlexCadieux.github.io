ab131bc59040dc0a465904a0adcb903aa69f8826
Alexandre Cadieux <cadieux.alex@gmail.com>
2017-05-25 15:49:48 -0400

    activated class on button

--- assets/javascript/handleButton.js
+++ assets/javascript/handleButton.js
@@ -22,3 +22,41 @@ $(function(){});
         scrollTop: $(".tools").offset().top - 80
     }, 2000);
   });
+
+  $(window).scroll(function(){
+      if($(document).scrollTop() > $(".about-me").offset().top - 81 && $(document).scrollTop() < $(".curriculum").offset().top - 80)
+      {
+        $('#aboutMeSection').addClass('activated');
+      }
+      else
+      {
+        $('#aboutMeSection').removeClass('activated')
+      }
+
+      if($(document).scrollTop() > $(".curriculum").offset().top - 81 && $(document).scrollTop() < $(".portfolio").offset().top - 80)
+      {
+        $('#cvSection').addClass('activated');
+      }
+      else
+      {
+        $('#cvSection').removeClass('activated')
+      }
+
+      if($(document).scrollTop() > $(".portfolio").offset().top - 81 && $(document).scrollTop() < $(".tools").offset().top - 150)
+      {
+        $('#portfolioSection').addClass('activated');
+      }
+      else
+      {
+        $('#portfolioSection').removeClass('activated')
+      }
+
+      if($(document).scrollTop() > $(".tools").offset().top - 151 && $(document).scrollTop() < $(".main-footer").offset().top - 80)
+      {
+        $('#toolsSection').addClass('activated');
+      }
+      else
+      {
+        $('#toolsSection').removeClass('activated')
+      }
+  });
